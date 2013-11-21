625351e1841aff97e7b338359147a9ee4d7d88b0
Catherine Sibert <siberc@rpi.edu>
2013-11-21 13:51:11 -0500

    Test of import TetrisSimulator

--- cross_entropy.py
+++ cross_entropy.py
@@ -1,1 +1,12 @@
-import simulator
+from simulator import TetrisSimulator
+
+#staring from Dellacherie model
+start_controller = [["landing_height",-1],
+                ["eroded_cells",1],
+                ["row_trans",-1],
+                ["col_trans",-1],
+                ["pits",-4],
+                ["cuml_wells",-1]]
+				
+sim_test = TetrisSimulator(controller = controller)
+sim_test.run()
