2c6a37fe06da29e721380012dc8b03515f88b1d6
John K. Lindstedt <john.k.lindstedt@gmail.com>
2013-11-21 14:17:53 -0500

    added function to generate a new controller from a base controller and a st.dev in a gaussian distribution

--- cross_entropy.py
+++ cross_entropy.py
@@ -1,4 +1,25 @@
 from simulator import TetrisSimulator
+import random
+
+
+###Functions
+def generate_controller(start, tols):
+    new_controller = []
+    
+    #for each feature
+    for i in range(0,len(start)):
+        #generate a new value around the mean
+        val = random.gauss(start[i][1],tols[i])
+        
+        #and add this pair to the new controller
+        new_controller.append([start[i][0],val])
+        
+    return new_controller
+
+
+
+
+###Script
 
 #staring from Dellacherie model
 target_controller = [["landing_height",-1],
@@ -17,8 +38,15 @@ start_controller = [["landing_height",0],
                 
 tolerances = [10,10,10,10,10,10]
 
+
+print(generate_controller(start_controller, tolerances))
+
 				
 sim_test = TetrisSimulator(controller = start_controller)
+
+
+
+
 
 
 result = sim_test.run()
