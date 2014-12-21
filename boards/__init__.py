341888e99d2af63ca65646e48a80629e1e8e85bd
Kevin P. Barry <barryk@rpi.edu>
2014-12-21 15:02:28 -0500

    minor import changes

--- boards/__init__.py
+++ boards/__init__.py
@@ -2,5 +2,7 @@ from tetris_zoid import *
 from tetris_cow import *
 from _helpers import print_board
 
-try: from tetris_cpp import tetris_cow2, set_cache_size, get_cache_size
-except ImportError: pass
+try: from tetris_cpp import tetris_cow2, set_row_cache_size, get_row_cache_size, set_board_cache_size, get_board_cache_size
+except ImportError as error:
+  import sys
+  print >> sys.stderr, 'ImportError:', str(error), '(tetris_cpp)'
