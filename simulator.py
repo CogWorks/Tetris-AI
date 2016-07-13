a5e207c93f9e550386e867095ddf7ea85cae78b9
Catherine Sibert <siberc@rpi.edu>
2016-07-13 09:54:12 -0400

    Update simulator.py
    Added mm_test2, first try at adding episode variable for mindmodelling

--- simulator.py
+++ simulator.py
@@ -744,3 +744,33 @@ def mm_test(landing_height, eroded_cells, row_trans, col_trans, pits, cuml_wells
 
 if __name__ == "__main__":
     print mm_test(1,1,1,1,1,1,1,1)
+
+def mm_test2(landing_height, eroded_cells, row_trans, col_trans, pits, cuml_wells, pit_depth, pit_rows, eps):
+
+    controller = {
+        "landing_height": landing_height,
+        "eroded_cells": eroded_cells,
+        "row_trans": row_trans,
+        "col_trans": col_trans,
+        "pits": pits,
+        "cuml_wells":cuml_wells,
+        "pit_depth": pit_depth,
+        "pit_rows": pit_rows
+    }
+
+    osim = TetrisSimulator(
+        controller=controller,
+            board=tetris_cow2(),
+            curr=all_zoids["L"],
+            next=all_zoids["S"],
+            show_choice=False,
+            show_scores=False,
+            show_options=False,
+            show_result=False,
+            seed=1
+    )
+
+    return osim.run(max_eps = eps)
+    
+if __name__ == "__main__":
+    print mm_test2(1,1,1,1,1,1,1,1)
