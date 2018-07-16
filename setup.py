db0278f2217175ee8833f30f88a0fa29d23956a3
Alec Bernardi <bernaa2@rpi.edu>
2018-07-16 16:26:31 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Tetris-AI',
-    version='2.0.dev',
+    version='2.0.dev1',
     packages=find_packages(),
     install_requires=[
         'numpy~=1.14',
