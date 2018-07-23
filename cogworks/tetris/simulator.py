5957ffe25ed945531191cd0dadb7a8e0e180ba8c
Alec Bernardi <bernaa2@rpi.edu>
2018-07-23 13:04:49 -0400

    Pull up move-policy by refactoring lookahead mechanism

--- cogworks/tetris/simulator.py
+++ cogworks/tetris/simulator.py
@@ -12,46 +12,70 @@ def move_drop(state, zoid):
                     row += 1
                 yield (rot, row, col)
 
+def policy_best(scorer, tie_breaker):
+    # Like builtin max, but returns all elements that have max value
+    def all_max(itr, key):
+        group = []
+        max = None
+        for e in itr:
+            k = key(e)
+            if max is None or max < k:
+                # e is new max, since previous max is strictly less
+                group = []
+                max = k
+            elif k < max:
+                # e cannot be a max, so skip it
+                continue
+            # k >= max
+            # e is a max, so add to group
+            group.append(e)
+        return group
 
-def simulate(state, zoid_gen, move_gen, scorer, tie_breaker, lookahead=1):
-
-    def best_future(state, zoids):
-        if len(zoids) == 0:
-            # No more zoids, so score the current state
-            return state, scorer(state)
-
-        # Compute futures from move_gen
-        futures = (state.future(zoids[0], *move) for move in move_gen(state, zoids[0]))
-
-        # Recursively evaluate futures to compute ultimate scores
-        futures = (best_future(future, zoids[1:]) for future in futures)
-
-        # Find the best options
-        choices = []
-        best = -float('inf')
-        for (future, score) in (f for f in futures if f is not None):
-            if score >= best:
-                if best < score:
-                    choices = []
-                    best = score
-                choices.append(future)
-
-        if len(choices) == 0:
+    def pick_best(futures):
+        maxes = all_max(futures, scorer)
+        if len(maxes) == 0:
             return None
-        elif len(choices) == 1:
-            return choices[0], best
+        elif len(maxes) == 1:
+            return maxes[0]
         else:
-            return tie_breaker(choices), best
+            return tie_breaker(maxes)
+    return pick_best
 
+def simulate(state, zoid_gen, move_gen, move_policy, lookahead=1):
+    assert lookahead > 0
     while True:
         # Slice out 'visible' zoids using lookahead
         zoids = tuple(itertools.islice(zoid_gen, 0, lookahead))
-        best = best_future(state, zoids)
-        if best is not None:
-            state = best[0]
-            yield state
+        # Create a stack of future pools
+        # First layer is simply the current state
+        pool_stack = [[state]]
+
+        # Fill remaining pool layers by generating possible futures from previous layer
+        for zoid in zoids:
+            pool_stack.append([
+                prev.future(zoid, *move)
+                for prev in pool_stack[-1]
+                for move in move_gen(prev, zoid)
+            ])
+
+        # Apply the move policy to the farthest non-empty future pool
+        next = None
+        while next is None and len(pool_stack) > 1:
+            pool = pool_stack.pop()
+            if pool:
+                next = move_policy(pool)
+
+        if next is not None:
+            # Backtrack to find first move on the path to this far-future, and yield it
+            while next.prev is not state:
+                next = next.prev
+            yield next
+
+            # Reset for the next iteration
+            state = next
+            # Push all yet-unplaced zoids back into front of zoid_gen, if any
             if len(zoids) > 1:
-                # Push all yet-unplaced zoids back into front of zoid_gen
                 zoid_gen = itertools.chain(zoids[1:], zoid_gen)
         else:
+            # There are no possible moves, so the simulation is over
             break
